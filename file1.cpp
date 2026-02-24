feb683177027176183de60ceb3c9c4607dbf87c5
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-02-24 19:45:03 +0700

    Menambahkan dekralasi variabel luas

--- file1.cpp
+++ file1.cpp
@@ -15,5 +15,21 @@ int main(){
 int main(){
     int p, l, luas;
 
-}
+    //langkah 2
+    cout << "Masukkan panjang = ";
+    cin >> p;
+
+    //langkah 3
+    cout << "Masukkan lebar = ";
+    cin >> l;
+
+    //langkah 4
+    luas = p * l;
+
+    //langkah 5
+    cout << "Hasilnya = " << luas << end1;
+    cout << "Terima kasih";
+}//selesai
+
+
 
